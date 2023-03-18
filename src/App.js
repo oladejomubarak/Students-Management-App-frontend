88ccb95eabcf85a3de01f40bb96f4025690a2773
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-18 09:53:56 +0000

    rendering data on page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import  React, {useState, useEffect} from 'react';
 import './App.css';
 import { getAllStudents } from './Client';
-import { Component } from 'react';
-
-// class App extends Component () {
-//   render(){
-//   getAllStudents().then(res => res.json().then(students => {
-//     console.log(students); 
-//   }));
-//   return <h1>Students Management Application</h1>
-// }
-// }
 
 function App() {
-    getAllStudents().then(res => res.json().then(students => {
-    console.log(students); 
-  }));
+  // state = {
+  //   students: []
+  // }
+  const [students, setStudents] = useState([]);
+
+  useEffect( () => fetchStudents);
+
+  const fetchStudents = () =>{
+    getAllStudents()
+    .then(res => res.json()
+    .then(students => {
+      //console.log(students)
+    setStudents({
+      students
+    });
+    
+    }));
+    
+  }     
+
+  //students = this.state
+
+    if (students && students.length){
+      return students.map((student, index) => {
+        return (
+          <div key={index}>
+            <h2>student.studentId</h2>
+            <p>student.firstName</p>
+            <p>student.lastName</p>
+            <p>student.email</p>
+          </div>
+        )
+      });
+    }
 
   return (
-    <h1>Students Management Application</h1>
+    
+    <h1>No student found</h1>
   )
 }
 
