f926b431b5dcc44ef8ba9680ffd6b38dd92bf38c
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-19 09:18:53 +0000

    rendering data on page in tables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import  React, {useState, useEffect} from 'react';
 import './App.css';
 import { getAllStudents } from './Client';
+import { 
+  Table 
+} from 'antd';
 
 function App() {
   // state = {
@@ -8,17 +11,14 @@ function App() {
   // }
   const [students, setStudents] = useState([]);
 
-  useEffect( () => fetchStudents);
+  useEffect(() =>{ fetchStudents();}, []);
 
   const fetchStudents = () =>{
     getAllStudents()
     .then(res => res.json()
     .then(students => {
       //console.log(students)
-    setStudents({
-      students
-    });
-    
+    setStudents(students);
     }));
     
   }     
@@ -26,21 +26,53 @@ function App() {
   //students = this.state
 
     if (students && students.length){
-      return students.map((student, index) => {
-        return (
-          <div key={index}>
-            <h2>student.studentId</h2>
-            <p>student.firstName</p>
-            <p>student.lastName</p>
-            <p>student.email</p>
-          </div>
-        )
-      });
+     const columns= [
+      {
+        title: "StudentId",
+        dataIndex: "StudentId",
+        key: "StudentId"
+      },
+      {
+        title: "First Name",
+        dataIndex: "firstName",
+        key: "firstName"
+      },
+      {
+        title: "Last Name",
+        dataIndex: "lastName",
+        key: "lastName"
+      },
+      {
+        title: "Email Address",
+        dataIndex: "email",
+        key: "email"
+      },
+      {
+        title: "Gender",
+        dataIndex: "gender",
+        key: "gender"
+      }
+     ];
     }
 
   return (
-    
     <h1>No student found</h1>
+    // <>
+    // {students.length == 0 ? 
+    // <h1>No student found</h1>
+    // :
+    // students.map((student, index) => {
+    //   return (
+    //     <div key={index}>
+    //       <h2>{student.studentId}</h2>
+    //       <p>{student.firstName}</p>
+    //       <p>{student.lastName}</p>
+    //       <p>{student.email}</p>
+    //     </div>
+
+    //   )
+    // })}
+    // </>
   )
 }
 
